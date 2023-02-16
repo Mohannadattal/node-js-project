fbf29bc28b4daadfdef4efea9a52f189018fdbd6
Mohannadgithub <mohannadattal85@gmail.com>
2023-02-16 13:52:18 +0100

    login Validation

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,15 @@ const User = require('../models/user');
 const router = express.Router();
 router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
-router.post('/login', authController.postLogin);
+router.post(
+  '/login',
+  [
+    body('email').isEmail().withMessage('Please enter a valid email address.'),
+    body('password', 'Password has to be valid.').isLength({ min: 12 })
+      .isAlphanumeric(),
+  ],
+  authController.postLogin
+);
 router.post(
   '/signup',
   [
@@ -14,10 +22,6 @@ router.post(
       .isEmail()
       .withMessage('Please enter a valid email.')
       .custom((value, { req }) => {
-        // if (value === 'test@test.com') {
-        //   throw new Error('This email address if forbidden');
-        // }
-        // return true;
         return User.findOne({ email: value }).then((userDoc) => {
           if (userDoc) {
             return Promise.reject(
