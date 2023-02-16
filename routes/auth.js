c7134637d6ddcc8111f000eca18cf27d2e469f29
Mohannadgithub <mohannadattal85@gmail.com>
2023-02-16 12:51:18 +0100

    Checking For Field Equality

--- routes/auth.js
+++ routes/auth.js
@@ -24,6 +24,12 @@ router.post(
     )
       .isLength({ min: 12 })
       .isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Password have be match!');
+      }
+      return true;
+    }),
   ],
   authController.postSignup
 );
