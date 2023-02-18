d73247f89152de0a7b0e7ab464cb4661454a2f17
Mohannadgithub <mohannadattal85@gmail.com>
2023-02-18 14:07:36 +0100

    Downloading Files with Authentication

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,10 +1,12 @@
 const Product = require('../models/product');
 const Order = require('../models/order');
+const fs = require('fs');
+const path = require('path');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then((products) => {
-     // console.log(products);
+      // console.log(products);
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
@@ -141,3 +143,15 @@ exports.getOrders = (req, res, next) => {
       return next(error);
     });
 };
+
+exports.getInvoice = (req,res,next) => {
+    const orderId = req.params.orderId;
+    const invoiceName = 'invoice-' + orderId + '.pdf';
+    const invoicePath = path.join('data', 'invoices', invoiceName);
+    fs.readFile(invoicePath, (err, data) => {
+      if (err) {
+        return next(err);
+      }
+      res.send(data);
+    });
+  };
