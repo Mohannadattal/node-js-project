0bc8e0972f1281f62aee3276da7dd402bbf230e2
Mohannadgithub <mohannadattal85@gmail.com>
2023-02-22 13:20:28 +0100

    Preparing Pagination Data on the Server

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,6 +5,7 @@ const path = require('path');
 const PDFDocument = require('pdfkit');
 
 const ITEMS_PER_PAGE = 2;
+let totalItems;
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -44,13 +45,24 @@ exports.getProduct = (req, res, next) => {
 exports.getIndex = (req, res, next) => {
   const page = req.query.page;
   Product.find()
-    .skip((page - 1) * ITEMS_PER_PAGE)
-    .limit(ITEMS_PER_PAGE)
+    .countDocuments()
+    .then((numberOfProducts) => {
+      totalItems = numberOfProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
+        totalProducts: totalItems,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage:Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch((err) => {
