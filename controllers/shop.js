053cea1b9a056889a03968bfb4d21b6b9a9a7863
Mohannadgithub <mohannadattal85@gmail.com>
2023-02-22 13:58:55 +0100

    Re-Using the Pagination Logic & Controls

--- controllers/shop.js
+++ controllers/shop.js
@@ -8,14 +8,26 @@ const ITEMS_PER_PAGE = 1;
 let totalItems;
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
   Product.find()
-
+    .countDocuments()
+    .then((numberOfProducts) => {
+      totalItems = numberOfProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
-      // console.log(products);
       res.render('shop/product-list', {
         prods: products,
-        pageTitle: 'All Products',
+        pageTitle: 'Products',
         path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage:Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch((err) => {
