1d21b080f65558b1d3261c1b97a349b22405f658
Mohannadgithub <mohannadattal85@gmail.com>
2023-02-24 13:27:57 +0100

    Manipulating the DOM

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,6 +1,7 @@
 const deleteProduct = (btn) => {
   prodId = btn.parentNode.querySelector('[name=productId]').value;
   const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+  const productElement = btn.closest('article');
   fetch('/admin/product/' + prodId, {
     method: 'DELETE',
     headers: {
@@ -8,7 +9,11 @@ const deleteProduct = (btn) => {
     },
   })
     .then((result) => {
-      console.log(result);
+      return result.json();
+    })
+    .then((data) => {
+      console.log(data);
+      productElement.parentNode.removeChild(productElement);
     })
     .catch((err) => {
       console.log(err);
